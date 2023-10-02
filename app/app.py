5b9b2f7ec05582aad1eebe5cc57a71db0b15dc28
vunguyenphanhoang <vunguyenphanhoang@yahoo.com>
2023-10-02 16:51:57 +0700

    change app 6

--- app/app.py
+++ app/app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # po = f'<h1> Captone Project of Udacity VUNPH </h1>'
-    # return po
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return f'Captone Project of Udacity VUNPH: {current_time}'
+    po = f'<h1> Captone Project of Udacity VUNPH </h1>'
+    return po
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
