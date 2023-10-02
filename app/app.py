b8d47664c9724a60e148902a198a6b411904869e
vunguyenphanhoang <vunguyenphanhoang@yahoo.com>
2023-10-02 16:20:44 +0700

    change app

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    po = "<h1> Captone Project of Udacity VUNPH </h1>"
-    return po.format(format)
+    po = f"<h1> Captone Project of Udacity VUNPH </h1>"
+    return po
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
